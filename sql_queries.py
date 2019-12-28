d07d6590ef9343de99ed92d41f55368f94724af0
Luciana Rocha <lucianaferreiradarocha@gmail.com>
2019-12-28 15:47:58 -0300

    feat: Write create and drop statements

--- sql_queries.py
+++ sql_queries.py
@@ -1,27 +1,43 @@
 # DROP TABLES
 
-songplay_table_drop = ""
-user_table_drop = ""
-song_table_drop = ""
-artist_table_drop = ""
-time_table_drop = ""
+songplay_table_drop = "DROP table IF EXISTS songplay;"
+user_table_drop = "DROP table IF EXISTS users;"
+song_table_drop = "DROP table IF EXISTS songs;"
+artist_table_drop = "DROP table IF EXISTS artists;"
+time_table_drop = "DROP table IF EXISTS time;"
 
 # CREATE TABLES
+ 
+songplay_table_create = (
+    "CREATE table IF NOT EXISTS songplay (" \
+        "songplay_id int, start_time int, user_id int," \
+        "level varchar, song_id varchar, artist_id int," \
+        "session_id int, location varchar, user_agent varchar)"
+    )
 
-songplay_table_create = ("""
-""")
+user_table_create = (
+    "CREATE table IF NOT EXISTS users (" \
+        "user_id int, first_name varchar, last_name varchar," \
+        "gender varchar, level varchar)"
+    )
 
-user_table_create = ("""
-""")
+song_table_create = (
+    "CREATE table IF NOT EXISTS songs (" \
+        "song_id varchar, title varchar, artist_id varchar," \
+        "year numeric, duration numeric)"
+    )
 
-song_table_create = ("""
-""")
+artist_table_create = (
+    "CREATE table IF NOT EXISTS artists (" \
+        "artist_id varchar, name varchar, location varchar," \
+        "latitude varchar, longitude varchar)"
+    )
 
-artist_table_create = ("""
-""")
-
-time_table_create = ("""
-""")
+time_table_create = (
+    "CREATE table IF NOT EXISTS time (" \
+        "start_time numeric, hour numeric, day numeric," \
+        "week numeric, month numeric, year numeric, weekday numeric)"
+    )
 
 # INSERT RECORDS
 
@@ -47,6 +63,5 @@ song_select = ("""
 """)
 
 # QUERY LISTS
-
 create_table_queries = [songplay_table_create, user_table_create, song_table_create, artist_table_create, time_table_create]
 drop_table_queries = [songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
